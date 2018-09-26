1cdc20cb50885d2872abc4d5427a662db1a44e28
SoloSagrado <SoloSagrado@Terminal>
2018-09-26 16:11:51 -0300

    adicionei uma nova linha

--- Erick GitHub/src/br/com/ericksilva/HelloGit.java
+++ Erick GitHub/src/br/com/ericksilva/HelloGit.java
@@ -11,6 +11,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Integrando o GitHub com o eclipes");
+		System.out.println("Minha bandeira nuca sera vemelha");
 
 	}
 
